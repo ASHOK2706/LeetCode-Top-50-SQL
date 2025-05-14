e60845983f0a655422efca1938a06418afe1d3d0
ASHOK2706 <ashokkumar27b@gmail.com>
2025-05-14 10:49:05 +0530

    sql

--- _47_SecondHighestSalary.sql
+++ _47_SecondHighestSalary.sql
@@ -4,3 +4,20 @@
     Write a solution to find the second highest distinct salary from the Employee table. If there is no second highest salary, 
     return nul
 */
+
+create table employee6(
+    id int primary key,
+    salary int 
+);
+
+insert into employee6(id, salary)
+values
+    (1, 100),
+    (2, 200),
+    (3, 300);
+
+-- Solution:
+
+select max(salary) as SecondHighestSalary 
+from employee6 
+where salary != (select max(salary) from employee6);
